db530f2c88dfdf2dce54f57e4f453abbe1232b10
Aneesh-2017 <pythonbootcamp100days@gmail.com>
2025-01-26 18:20:20 +0530

    Delete index.py

--- index.py
+++
@@ -1,4 +0,0 @@
-print("Welcome to the Band Name Generator.")
-name_of_city = input("What's the name of the city you grew up in? \n")
-name_of_pet = input("What's your Pet's Name? \n")
-print("Your Band name could be " + name_of_city + " " + name_of_pet)
